41584468ac2541bec063e6fce58b17b679f54157
JiHun-star <rlarkspx@gmail.com>
2022-09-20 19:47:11 +0900

    프로필이미지

--- src/routes/producers/+page.svelte
+++ src/routes/producers/+page.svelte
@@ -2,5 +2,13 @@
 	<section>
 		<h1>VO!CE PRODUCERS</h1>
 		<h1>준비중 입니다 !!</h1>
+		<img src="profile/1.png" alt="별다소니" />
 	</section>
 </main>
+
+<style>
+	img {
+		width: 88px;
+		height: 88px;
+	}
+</style>
