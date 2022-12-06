590f7d6313e690d74361388323399dc232f69a0b
JiHun-star <rlarkspx@gmail.com>
2022-12-06 18:59:16 +0900

    이름변경

--- src/routes/cooperation/+page.svelte
+++ src/routes/cooperation/+page.svelte
@@ -13,7 +13,7 @@
 		<article class="flex flex-wrap flex-row justify-center gap-5">
 			<div class="card w-80 bg-base-content shadow-xl lg:w-96">
 				<div class="card-body text-stone-700">
-					<h2 class="card-title">한국 서브컬쳐 연합회</h2>
+					<h2 class="card-title">ON STAGE</h2>
 					<p class="text-left indent-2">
 						한국 서브컬쳐 분야에서 활동하시는 다양한 분들이 교류하고 있는 커뮤니티 입니다.
 					</p>
