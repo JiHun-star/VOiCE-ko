d48c1db226617c74911bb9fad6d560a6e4590630
JiHun-star <rlarkspx@gmail.com>
2023-02-01 20:40:15 +0900

    1월

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,12 +12,12 @@
 	<section class="monthly">
 		<h1>
 			VO!CE CHART
-			<br />2022.12
+			<br />2023.01
 		</h1>
 		<br />
 		<iframe
 			class="rounded-2xl border-4 border-primary"
-			src="https://www.youtube.com/embed/fkAqTPXW3lY"
+			src="https://www.youtube.com/embed/sExm-SoSLKA"
 			title="YouTube video player"
 			frameborder="0"
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
